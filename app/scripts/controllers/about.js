2fe5621b313694235efe7492622821a3ed9c91ef
unknown <visiongithub@gmail.com>
2017-04-08 13:06:01 +0530

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the smsApp
+ */
+angular.module('smsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smsApp
+ */
+angular.module('smsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
